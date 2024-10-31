8cc87c878e8297343167aaf4265d4110d0e45bff
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2024-10-31 22:13:28 +0100

    integrate category page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'antd';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {DashboardLayout} from "./layouts/DashoboardLayout/DashboardLayout";
+import {CategoryPage} from "./pages/CategoryPage/CategoryPage";
+import {HomePage} from "./pages/HomePage/HomePage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button type={"primary"} size={"small"}> Test </Button>
-      </header>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<DashboardLayout />} >
+                    <Route path="" element={<HomePage/>} />
+                    <Route path="category" element={<CategoryPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,8 @@
+import './HomePage.css'
+
+
+export const HomePage = () => {
+    return (
+        <div>Home Page</div>
+    )
+}
